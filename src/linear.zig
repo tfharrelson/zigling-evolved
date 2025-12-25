798cdc4cc8e01c55f40f3e423c696ff75abe237a
tfharrelson <tharrel4@gmail.com>
2025-12-24 18:53:15 -0800

    linear component

---
+++ src/linear.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const Tensor = @import("tensor.zig").Tensor;
+const Allocator = std.mem.Allocator;
+
+pub fn Linear(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        shape: *[2]usize,
+        params: Tensor(T),
+
+        pub fn init(alloc: Allocator, shape: *[2]usize, seed: ?usize) !Self {
+            var random_seed: usize = undefined;
+            if (seed) |s| {
+                random_seed = s;
+            } else {
+                try std.posix.getrandom(std.mem.asBytes(&random_seed));
+            }
+            var pnrg = std.Random.DefaultPrng.init(random_seed);
+            const rand = pnrg.random();
+            var num_elems: usize = 1;
+            for (shape) |s| {
+                num_elems *= s;
+            }
+            var arr: std.ArrayList(T) = try .initCapacity(alloc, num_elems);
+            for (0..num_elems) |_| {
+                try arr.append(alloc, rand.float(T));
+            }
+            // TODO: should this be const? this change during training
+            const params = try Tensor(T).init(arr.items, shape);
+            return Self{ .shape = shape, .params = params };
+        }
+
+        pub fn forward(self: *Self, alloc: Allocator, state: Tensor(T)) !Tensor(T) {
+            return try self.params.matmul(alloc, state);
+        }
+    };
+}
+
+test "linear matmul happy path" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]f32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    const t = try Tensor(f32).init(&elements, &shape);
+    var l = try Linear(f32).init(allocator, &shape, null);
+
+    const output = try l.forward(allocator, t);
+    const exp_shape = [_]usize{ 2, 2 };
+    for (exp_shape, output.shape) |e, s| {
+        try std.testing.expect(e == s);
+    }
+}
