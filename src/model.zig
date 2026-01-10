e72c402d1f2263c8de79a7ceba67af5a98f08ac6
tfharrelson <tharrel4@gmail.com>
2026-01-09 21:42:24 -0800

    feat: backprop poc - still need better testing

--- src/model.zig
+++ src/model.zig
@@ -2,36 +2,53 @@ const std = @import("std");
 const Tensor = @import("tensor.zig").Tensor;
 const TensorError = @import("tensor.zig").TensorError;
 const Linear = @import("linear.zig").Linear;
+const DSilu = @import("tensor.zig").DSilu;
 const Silu = @import("tensor.zig").Silu;
+const Index = @import("tensor.zig").Index;
 const Allocator = std.mem.Allocator;
 
 pub fn Model(comptime T: type) type {
     return struct {
         const Self = @This();
         ptr: *anyopaque,
+        vtable: *const VTable,
 
-        forwardFn: *const fn (ptr: *anyopaque, alloc: Allocator, tensor: *Tensor(T)) TensorError!Tensor(T),
+        const VTable = struct {
+            forward: *const fn (ptr: *anyopaque, alloc: Allocator, tensor: *Tensor(T)) TensorError!Tensor(T),
+            backward: *const fn (ptr: *anyopaque, alloc: Allocator, tensor: *Tensor(T), learning_rate: T) TensorError!Tensor(T),
+        };
 
         pub fn init(ptr: anytype) Self {
             const typ = @TypeOf(ptr);
             const ptr_info = @typeInfo(typ);
             std.debug.assert(ptr_info == .pointer); // must be a pointer
 
-            const raw_struct = struct {
+            const gen = struct {
                 pub fn forward(pointy: *anyopaque, alloc: Allocator, tensor: *Tensor(T)) TensorError!Tensor(T) {
                     const self: typ = @ptrCast(@alignCast(pointy));
                     return ptr_info.pointer.child.forward(self, alloc, tensor);
+                }
+                pub fn backward(pointy: *anyopaque, alloc: Allocator, tensor: *Tensor(T), learning_rate: T) TensorError!Tensor(T) {
+                    const self: typ = @ptrCast(@alignCast(pointy));
+                    return ptr_info.pointer.child.backward(self, alloc, tensor, learning_rate);
                 }
             };
 
             return .{
                 .ptr = ptr,
-                .forwardFn = raw_struct.forward,
+                .vtable = &.{
+                    .forward = gen.forward,
+                    .backward = gen.backward,
+                },
             };
         }
 
         pub fn forward(self: Self, alloc: Allocator, tensor: *Tensor(T)) TensorError!Tensor(T) {
-            return self.forwardFn(self.ptr, alloc, tensor);
+            return self.vtable.forward(self.ptr, alloc, tensor);
+        }
+
+        pub fn backward(self: Self, alloc: Allocator, tensor: *Tensor(T), learning_rate: T) TensorError!Tensor(T) {
+            return self.vtable.backward(self.ptr, alloc, tensor, learning_rate);
         }
     };
 }
@@ -46,14 +63,70 @@ pub fn Model(comptime T: type) type {
 pub fn FullyConnectedLayer(comptime T: type) type {
     return struct {
         const Self = @This();
+        pub const Cache = struct {
+            grad: Tensor(T),
+            input: Tensor(T),
+        };
         linear: Linear(T),
         // TODO: allow for generic activation functions
+        store_grad: bool = true,
+        cache: ?Cache = null,
 
         pub fn forward(self: *Self, alloc: Allocator, tensor: *Tensor(T)) TensorError!Tensor(T) {
-            // const self: *Self = @ptrCast(@alignCast(ptr));
-            // TODO: sort out how to throw away intermediate states that are not to be stored anywhere
             var intermediate = try self.linear.forward(alloc, tensor);
+            if (self.store_grad) {
+                self.cache = .{
+                    .grad = try DSilu(T, alloc, &intermediate),
+                    .input = tensor.*,
+                };
+            }
             return try Silu(T, alloc, &intermediate);
+        }
+
+        pub fn backward(self: *Self, alloc: Allocator, tensor: *Tensor(T), learning_rate: T) TensorError!Tensor(T) {
+            if (self.cache) |*c| {
+                // update the weights automatically? pytorch separates backward from step
+                // but i'm having trouble envisioning a situation where those should be decoupled
+                // so i'm tentatively going to couple them by default here and regret some mistake later
+
+                // need to check whether or not the caller input a tensor batch
+                // if they did, then we have to map over the batch dimension and outer product each
+                if (tensor.shape.len == 1) {
+                    try tensor.unsqueeze(alloc, 0);
+                    try c.input.unsqueeze(alloc, 0);
+                }
+                if (tensor.shape[0] != c.input.shape[0]) {
+                    return TensorError.IncompatibleShapeError;
+                }
+
+                // outer product - so unsqueeze edges
+                // the first dimension is always the batch index now
+                // and we want to keep it that way, so unsqueeze the first non-batch index
+                try tensor.unsqueeze(alloc, 1);
+                try c.input.unsqueeze(alloc, -1);
+                var total_weight_grad: Tensor(T) = try .zeros(alloc, self.linear.shape);
+                for (0..tensor.shape[0]) |i| {
+                    var indices = [_]Index{ .{ .int = i }, .{ .all = {} }, .{ .all = {} } };
+                    var t = try tensor.get(alloc, &indices);
+                    var ci = try c.input.get(alloc, &indices);
+
+                    var weight_grad = try ci.matmul(alloc, &t);
+                    weight_grad.mul(learning_rate);
+                    try total_weight_grad.add(&weight_grad);
+                }
+                try self.linear.params.add(&total_weight_grad);
+                var delta = try tensor.matmul(alloc, &self.linear.params);
+                // TODO: make an element-wise multiplication method on tensor
+                // TODO: figure out how to manipulate the tensor object directly to save memory
+                for (0..c.grad.items.len) |i| {
+                    delta.items[i] *= c.grad.items[i];
+                }
+                return delta;
+            } else {
+                // TODO: come up with a sensible error handling strategy for this one
+                // it's not quite a tensor error so i have to merge tensor errors with layer errors
+                return TensorError.UnexpectedError;
+            }
         }
 
         pub fn model(self: *Self) Model(T) {
@@ -62,7 +135,7 @@ pub fn FullyConnectedLayer(comptime T: type) type {
     };
 }
 
-test "fcl happy path" {
+test "fcl model forward happy path" {
     const allocator = std.heap.page_allocator;
     var elements = [_]f32{ 1, 2, 3, 4 };
     var shape = [_]usize{ 2, 2 };
@@ -71,6 +144,22 @@ test "fcl happy path" {
     const l = try Linear(f32).init(allocator, &shape, null);
     var m = FullyConnectedLayer(f32){ .linear = l };
     const output = try m.model().forward(allocator, &t);
+    const exp_shape = [_]usize{ 2, 2 };
+    for (exp_shape, output.shape) |e, s| {
+        try std.testing.expect(e == s);
+    }
+}
+
+test "fcl model backward happy path" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]f32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t = try Tensor(f32).init(&elements, &shape);
+    const l = try Linear(f32).init(allocator, &shape, null);
+    var m = FullyConnectedLayer(f32){ .linear = l };
+    _ = try m.model().forward(allocator, &t);
+    const output = try m.model().backward(allocator, &t, 0.01);
     const exp_shape = [_]usize{ 2, 2 };
     for (exp_shape, output.shape) |e, s| {
         try std.testing.expect(e == s);
--- src/tensor.zig
+++ src/tensor.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 const math = std.math;
+const Allocator = std.mem.Allocator;
 
 pub const TensorError = error{
     IncompatibleShapeError,
@@ -14,7 +15,7 @@ pub const TensorError = error{
 
 const IndexTag = enum { all, int };
 
-const Index = union(IndexTag) {
+pub const Index = union(IndexTag) {
     all: void,
     int: usize,
 };
@@ -37,13 +38,27 @@ pub fn Tensor(comptime T: type) type {
                 return TensorError.HighDimensionality;
             }
             if (items.len != expected_num_items) {
-                std.debug.print("Unexpected number of items {d} found given shape {d}.\n", .{ items.len, expected_num_items });
                 return TensorError.IncompatibleShapeError;
             }
             return .{ .shape = shape, .items = items };
         }
 
-        pub fn get(self: *const Self, allocator: std.mem.Allocator, indices: []Index) TensorError!Tensor(T) {
+        pub fn zeros(allocator: Allocator, shape: []usize) TensorError!Self {
+            if (shape.len > 10) {
+                return TensorError.HighDimensionality;
+            }
+            var size: usize = 1;
+            for (shape) |s| {
+                size *= s;
+            }
+            var zeros_list = std.ArrayList(T).initCapacity(allocator, size) catch return TensorError.OutOfMemory;
+            for (0..size) |_| {
+                zeros_list.append(allocator, 0) catch return TensorError.OutOfMemory;
+            }
+            return .{ .shape = shape, .items = zeros_list.items };
+        }
+
+        pub fn get(self: *const Self, allocator: Allocator, indices: []Index) TensorError!Tensor(T) {
             if (indices.len != self.shape.len) {
                 return TensorError.IncompatibleShapeError;
             }
@@ -61,13 +76,12 @@ pub fn Tensor(comptime T: type) type {
                         if (idx >= s) {
                             return TensorError.OutOfBounds;
                         }
-                        num_elems *= 1;
                     },
                 }
             }
 
             var inner_idx: u32 = 0;
-            var new_shape: std.ArrayList(usize) = .empty;
+            var new_shape = std.ArrayList(usize).initCapacity(allocator, dim) catch return TensorError.OutOfMemory;
             var offset: usize = 0;
             var curr_weight: usize = 1;
             var weights: std.ArrayList(usize) = .empty;
@@ -78,7 +92,8 @@ pub fn Tensor(comptime T: type) type {
                 const rev_idx = indices.len - 1 - i;
                 switch (indices[rev_idx]) {
                     IndexTag.all => {
-                        new_shape.append(allocator, self.shape[rev_idx]) catch return TensorError.OutOfMemory;
+                        new_shape.insertAssumeCapacity(0, self.shape[rev_idx]);
+                        // new_shape.append(allocator, self.shape[rev_idx]) catch return TensorError.OutOfMemory;
                         weights.append(allocator, curr_weight) catch return TensorError.OutOfMemory;
                         inner_idx += 1;
                     },
@@ -89,13 +104,25 @@ pub fn Tensor(comptime T: type) type {
                 curr_weight *= self.shape[rev_idx];
             }
 
+            // wait wait wait
+            // don't need any weights during the loop, just need to get the indices of the contracted matrix, e.g.
+            // if i have a 6x5x4x3x2 tensor and i want a slice over [2,All,0,All,1], then I just need the indices
+            // within the 5x3 inner matrix. The algorithm for that is much much simpler. It's a two step using
+            // modulo and floordivide. i % N_1 = index_1, floorDivide(i, N_1) => i, loop over the dimensionality
+            // of the inner matrix. Then once we get all indices, apply weights to each index value, sum them up
+            // add the offset and you have an index in the high dimensional `elements` array on self.
+            // NOTE: the weights list is already in reverse, the new_shape list is in the correct orientation
+            // TODO: should try to figure out how to clean up the reverse vs non-reverse logic in this part.
+            // good news though, we have decent tests for making this a bit safer
             var elements: std.ArrayList(T) = .empty;
             for (0..num_elems) |i| {
                 var elem_idx: usize = 0;
                 var floor_idx: usize = i;
                 for (0..new_shape.items.len) |j| {
                     const rev_idx = new_shape.items.len - 1 - j;
-                    elem_idx += @mod(i, new_shape.items[rev_idx]) * weights.items[rev_idx];
+                    // elem_idx += @mod(i, new_shape.items[rev_idx]) * weights.items[rev_idx];
+                    // floor_idx = @divFloor(floor_idx, new_shape.items[rev_idx]);
+                    elem_idx += @mod(floor_idx, new_shape.items[rev_idx]) * weights.items[j];
                     floor_idx = @divFloor(floor_idx, new_shape.items[rev_idx]);
                 }
                 elements.append(allocator, self.items[elem_idx + offset]) catch return TensorError.OutOfMemory;
@@ -103,7 +130,7 @@ pub fn Tensor(comptime T: type) type {
             return Tensor(T).init(elements.items, new_shape.items) catch unreachable;
         }
 
-        pub fn matmul(self: *Self, allocator: std.mem.Allocator, other: *Tensor(T)) TensorError!Tensor(T) {
+        pub fn matmul(self: *Self, allocator: Allocator, other: *Tensor(T)) TensorError!Tensor(T) {
             // standard matrix multiplication that takes the last dim
             // of self and multiplies with the first dim of other.
             if (self.shape[self.shape.len - 1] != other.shape[0]) {
@@ -115,7 +142,7 @@ pub fn Tensor(comptime T: type) type {
                 self_num_items *= s;
             }
             var other_num_items: usize = 1;
-            for (other.shape[0 .. other.shape.len - 1]) |s| {
+            for (other.shape[1..other.shape.len]) |s| {
                 other_num_items *= s;
             }
 
@@ -127,7 +154,7 @@ pub fn Tensor(comptime T: type) type {
                     // TODO: handle the single dimension case more gracefully
                     var rest_other_index_list: std.ArrayList(Index) = .empty;
                     if (other.shape.len != 1) {
-                        rest_other_index_list = try Tensor(T).getIndexList(allocator, j, other.shape[0 .. other.shape.len - 1]);
+                        rest_other_index_list = try Tensor(T).getIndexList(allocator, j, other.shape[1..other.shape.len]);
                     }
                     self_index_list.append(allocator, .{ .all = {} }) catch return TensorError.OutOfMemory;
                     var other_index_list: std.ArrayList(Index) = .empty;
@@ -157,41 +184,30 @@ pub fn Tensor(comptime T: type) type {
             return Tensor(T).init(output_elements.items, new_shape_list.items) catch unreachable;
         }
 
-        fn getIndexList(allocator: std.mem.Allocator, element_idx: usize, shape: []usize) TensorError!std.ArrayList(Index) {
+        fn getIndexList(allocator: Allocator, element_idx: usize, shape: []usize) TensorError!std.ArrayList(Index) {
             // get the array indices of a specific element index in the 'items' list
             var output_list: std.ArrayList(Index) = .empty;
+            // can't i just: for elem 11 in a 4x5 matrix
+            // 11 floor 5 = 2 check
+            // (11 - 10) floor 1 = 1 check
+            // for elem 3 in a 2x2 matrix
+            // 3 floor 2 = 1 check
+            // 3 - 2*1 floor 1 = 1 check
             var size: usize = 1;
             for (shape) |s| {
                 size *= s;
             }
-            // element 11 in a 4x5 matrix is elem (2, 1)
-            // can i do whatever mod math in any order to figure out the index?
-            // 11 % 4 = 3 -> x
-            // 11 % 5 = 1 -> check
-            // 11 - 1 % 4 = 2 -> check
-            // so no, the order does matter and has to be done in reverse
-            for (0..shape.len) |i| {
-                const s = shape[shape.len - 1 - i];
-                output_list.append(allocator, .{ .int = @mod(element_idx, size) }) catch return TensorError.OutOfMemory;
+            var remaining_elems = element_idx;
+            for (shape) |s| {
                 size /= s;
-            }
-            // inplace reverse the list
-            // TODO: remove this nonsense once i sort out switching to column major ordering
-            // i'm just doing too many reverses for this to make sense
-            var left: usize = 0;
-            if (output_list.items.len == 0) {
-                return TensorError.UnexpectedError;
-            }
-            var right: usize = output_list.items.len - 1;
-            while (left < right) {
-                const left_cache = output_list.items[left];
-                output_list.items[left] = output_list.items[right];
-                output_list.items[right] = left_cache;
-                left += 1;
-                right -= 1;
+                const index = @divFloor(remaining_elems, size);
+                // const index = @mod(remaining_elems, s);
+                output_list.append(allocator, .{ .int = index }) catch return TensorError.OutOfMemory;
+                remaining_elems -= index * size;
             }
             return output_list;
         }
+
         pub fn argmax(self: *Self) TensorError!usize {
             // TODO: generalize this to use any shape - i have a feeling this may be necessary at some point
             if (self.shape.len != 1) {
@@ -207,18 +223,64 @@ pub fn Tensor(comptime T: type) type {
             }
             return max_index;
         }
+
+        pub fn unsqueeze(self: *Self, alloc: Allocator, dim: isize) TensorError!void {
+            const udim = @abs(dim);
+            const slice_dim = if (dim < 0) self.shape.len - udim + 1 else udim;
+            const left: []usize = self.shape[0..slice_dim];
+            const right: []usize = self.shape[slice_dim..];
+            var new_shape = std.ArrayList(usize).initCapacity(alloc, self.shape.len + 1) catch return TensorError.OutOfMemory;
+            new_shape.appendSlice(alloc, left) catch return TensorError.OutOfMemory;
+            new_shape.append(alloc, 1) catch return TensorError.OutOfMemory;
+            new_shape.appendSlice(alloc, right) catch return TensorError.OutOfMemory;
+            self.shape = new_shape.items;
+        }
+
+        pub fn mul(self: *Self, scalar: T) void {
+            // scalar multiplication that mutates self
+            for (0..self.items.len) |i| {
+                self.items[i] = self.items[i] * scalar;
+            }
+        }
+
+        pub fn add(self: *Self, other: *Tensor(T)) TensorError!void {
+            // tensor addition between compatible shapes that mutates self
+            for (self.shape, other.shape) |ss, os| {
+                if (ss != os) {
+                    return TensorError.IncompatibleShapeError;
+                }
+            }
+            for (0..self.items.len) |i| {
+                self.items[i] += other.items[i];
+            }
+        }
     };
 }
 
-pub fn Silu(comptime T: type, allocator: std.mem.Allocator, t: *Tensor(T)) TensorError!Tensor(T) {
+pub fn Silu(comptime T: type, allocator: Allocator, t: *Tensor(T)) TensorError!Tensor(T) {
     // TODO: revisit the allocation semantics - it would be way more efficient to activate
     // tensor values in place. but right now it's just easier to allocate more memory for every op
     // just to get things working.
     var activated_items: std.ArrayList(T) = .empty;
     for (t.items) |i| {
-        activated_items.append(allocator, i / (1 + math.exp(-i))) catch return TensorError.OutOfMemory;
+        activated_items.append(allocator, i * sigmoid(T, i)) catch return TensorError.OutOfMemory;
     }
     return Tensor(T).init(activated_items.items, t.shape) catch unreachable;
+}
+
+inline fn sigmoid(comptime T: type, val: T) T {
+    return 1 / (1 + math.exp(-val));
+}
+
+pub fn DSilu(comptime T: type, allocator: Allocator, t: *Tensor(T)) TensorError!Tensor(T) {
+    // derivative of the silu function
+    var derivative_items: std.ArrayList(T) = .empty;
+    var s: T = undefined;
+    for (t.items) |i| {
+        s = sigmoid(T, i);
+        derivative_items.append(allocator, s + i * (s * (1 - s))) catch return TensorError.OutOfMemory;
+    }
+    return Tensor(T).init(derivative_items.items, t.shape) catch unreachable;
 }
 
 test "tensor init" {
@@ -275,6 +337,22 @@ test "tensor get all" {
     try expect(t2.items[1] == 5);
 }
 
+test "tensor get all 3d" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]u32{ 1, 2, 3, 4, 5, 6 };
+    var shape = [_]usize{ 1, 2, 3 };
+
+    var t = try Tensor(u32).init(&elements, &shape);
+    var indices = [_]Index{ .{ .int = 0 }, .{ .all = {} }, .{ .all = {} } };
+    const t2 = try t.get(allocator, &indices);
+    for (0..t2.shape.len) |i| {
+        try expect(t2.shape[i] == t.shape[i + 1]);
+    }
+    for (0..elements.len) |i| {
+        try expect(t2.items[i] == elements[i]);
+    }
+}
+
 test "tensor get errors" {
     const allocator = std.heap.page_allocator;
     var elements = [_]u32{ 1, 2, 3, 4, 5, 6 };
@@ -291,6 +369,21 @@ test "tensor get errors" {
     try std.testing.expectError(TensorError.IncompatibleShapeError, shape_error);
 }
 
+test "tensor get index list 3d" {
+    const allocator = std.heap.page_allocator;
+
+    var shape = [_]usize{ 1, 2, 2 };
+    const index_list = try Tensor(u32).getIndexList(allocator, 3, &shape);
+    const exp_items = [_]Index{ .{ .int = 0 }, .{ .int = 1 }, .{ .int = 1 } };
+    try expect(index_list.items.len == exp_items.len);
+    for (index_list.items, exp_items) |i, e| {
+        expect(i.int == e.int) catch {
+            std.debug.print("failed, index_list = {any}, expected = {any}\n", .{ index_list, exp_items });
+            return error.TestUnexpectedError;
+        };
+    }
+}
+
 test "tensor matmul happy path" {
     const allocator = std.heap.page_allocator;
     var elements = [_]u32{ 1, 2, 3, 4 };
@@ -304,6 +397,26 @@ test "tensor matmul happy path" {
         try std.testing.expect(e == s);
     }
     const exp_items = [_]u32{ 7, 10, 15, 22 };
+    for (exp_items, output.items) |e, i| {
+        try std.testing.expect(e == i);
+    }
+}
+
+test "tensor matmul vectors" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]u32{ 1, 2 };
+    var shape1 = [_]usize{ 1, 2 };
+    var shape2 = [_]usize{ 2, 1 };
+
+    var t1 = try Tensor(u32).init(&elements, &shape1);
+    var t2 = try Tensor(u32).init(&elements, &shape2);
+
+    const output = try t2.matmul(allocator, &t1);
+    const exp_shape = [_]usize{ 2, 2 };
+    for (exp_shape, output.shape) |e, s| {
+        try std.testing.expect(e == s);
+    }
+    const exp_items = [_]u32{ 1, 2, 2, 4 };
     for (exp_items, output.items) |e, i| {
         try std.testing.expect(e == i);
     }
@@ -325,6 +438,22 @@ test "tensor silu" {
     try expect(activated_t.items[1] < 10 and activated_t.items[1] > 9.99);
 }
 
+test "tensor dsilu" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]f32{ -10, 10 };
+    var shape = [_]usize{2};
+
+    var t = try Tensor(f32).init(&elements, &shape);
+
+    const derivative_t = try DSilu(f32, allocator, &t);
+
+    try expect(derivative_t.shape.len == 1);
+    try expect(derivative_t.items.len == 2);
+
+    try expect(derivative_t.items[0] < 0 and derivative_t.items[0] > -0.01);
+    try expect(derivative_t.items[1] < 1.01 and derivative_t.items[1] > 0.99);
+}
+
 test "tensor argmax" {
     var elements = [_]f32{ -10, 10, 0.4 };
     var shape = [_]usize{3};
@@ -333,4 +462,99 @@ test "tensor argmax" {
 
     const res = try t.argmax();
     try expect(res == 1);
+}
+
+test "tensor unsqueeze left" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]u32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t = try Tensor(u32).init(&elements, &shape);
+
+    try t.unsqueeze(allocator, 0);
+    const exp_shape = [_]usize{ 1, 2, 2 };
+    for (exp_shape, t.shape) |e, s| {
+        try std.testing.expect(e == s);
+    }
+    const exp_items = [_]u32{ 1, 2, 3, 4 };
+    for (exp_items, t.items) |e, i| {
+        try std.testing.expect(e == i);
+    }
+}
+
+test "tensor unsqueeze right" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]u32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t = try Tensor(u32).init(&elements, &shape);
+
+    try t.unsqueeze(allocator, -1);
+    const exp_shape = [_]usize{ 2, 2, 1 };
+    for (exp_shape, t.shape) |e, s| {
+        try std.testing.expect(e == s);
+    }
+    const exp_items = [_]u32{ 1, 2, 3, 4 };
+    for (exp_items, t.items) |e, i| {
+        try std.testing.expect(e == i);
+    }
+}
+
+test "tensor unsqueeze middle" {
+    const allocator = std.heap.page_allocator;
+    var elements = [_]u32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t = try Tensor(u32).init(&elements, &shape);
+
+    try t.unsqueeze(allocator, 1);
+    const exp_shape = [_]usize{ 2, 1, 2 };
+    for (exp_shape, t.shape) |e, s| {
+        try std.testing.expect(e == s);
+    }
+    const exp_items = [_]u32{ 1, 2, 3, 4 };
+    for (exp_items, t.items) |e, i| {
+        try std.testing.expect(e == i);
+    }
+}
+
+test "tensor scalar mul" {
+    var elements = [_]u32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t = try Tensor(u32).init(&elements, &shape);
+    t.mul(2);
+    const exp_elems = [_]u32{ 2, 4, 6, 8 };
+    for (exp_elems, t.items) |e, i| {
+        try expect(e == i);
+    }
+}
+
+test "tensor add happy path" {
+    var elements = [_]u32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t1 = try Tensor(u32).init(&elements, &shape);
+
+    var elements2 = [_]u32{ 2, 3, 4, 5 };
+    var t2 = try Tensor(u32).init(&elements2, &shape);
+    try t1.add(&t2);
+    const exp_elems = [_]u32{ 3, 5, 7, 9 };
+    for (exp_elems, t1.items) |e, i| {
+        try expect(e == i);
+    }
+}
+
+test "tensor add shape error" {
+    var elements = [_]u32{ 1, 2, 3, 4 };
+    var shape = [_]usize{ 2, 2 };
+
+    var t1 = try Tensor(u32).init(&elements, &shape);
+
+    var elements2 = [_]u32{ 2, 3, 4, 5, 6, 7 };
+    var shape2 = [_]usize{ 2, 3 };
+    var t2 = try Tensor(u32).init(&elements2, &shape2);
+    const res = t1.add(&t2);
+
+    try std.testing.expectError(TensorError.IncompatibleShapeError, res);
 }
